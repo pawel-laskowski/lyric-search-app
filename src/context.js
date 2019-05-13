64e22587a190dc9213e1e124e36bac68009a54b9
Pawel Laskowski <pawel-laskowski@outlook.com>
2019-05-13 21:12:57 +0200

    Add Spinner and Track components

--- src/context.js
+++ src/context.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 
 const Context = React.createContext()
 
 export class Provider extends React.Component {
     state = {
-        track_list: [
-            { track: { track_name: 'abc' }},
-            { track: { track_name: '123' } }
-        ],
+        track_list: [],
         heading: 'Top ten tracks'
     }
+
+    componentDidMount() {
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => {
+                // console.log(res.data)
+                this.setState({track_list: res.data.message.body.track_list})
+            })
+            .catch(err => console.log(err))
+    }
+
+
   render() {
     return (
         <Context.Provider value={this.state}>
